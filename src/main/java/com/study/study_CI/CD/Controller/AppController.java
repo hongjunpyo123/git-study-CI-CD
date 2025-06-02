3258ab4a271c45d1cbd922a26b856c40203a70e4
june <0721hjp@naver.com>
2025-06-02 22:28:06 +0900

    AppController 추가

---
+++ src/main/java/com/study/study_CI/CD/Controller/AppController.java
@@ -0,0 +1,13 @@
+package com.study.study_CI.CD.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello world!";
+    }
+}
